98f9a2646d1ab55e4a3a24014c39e5e09493d895
Korlan Omarova <omar_korlan@mail.ru>
2021-02-04 10:45:56 +0600

    records-comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AddRecordComponent } from './add-record/add-record.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddRecordComponent
+    AddRecordComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule

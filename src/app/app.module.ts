2309b246f3b8af6612a6a4f3bfeb84d526fd6daa
Korlan Omarova <omar_korlan@mail.ru>
2021-02-04 10:12:31 +0600

    add-record

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddRecordComponent } from './add-record/add-record.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecordComponent
   ],
   imports: [
     BrowserModule
